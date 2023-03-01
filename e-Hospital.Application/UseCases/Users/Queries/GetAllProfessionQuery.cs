2f02b301352f63a9c0c90011d32b804ca837cda4
Bekzod <shamsiyevbekzod368@gmail.com>
2023-03-02 01:19:52 +0500

    Profession CRUD

---
+++ e-Hospital.Application/UseCases/Users/Queries/GetAllProfessionQuery.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using e_Hospital.Application.Abstractions;
+using e_Hospital.Application.DTOs;
+using Microsoft.EntityFrameworkCore;
+
+namespace e_Hospital.Application.UseCases.Users.Queries
+{
+    public class GetAllProfessionQuery : IQuery<List<ProfessionViewModel>>
+    {
+    }
+
+    public class GetAllProfessionQueryHandler : IQueryHandler<GetAllProfessionQuery, List<ProfessionViewModel>>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public GetAllProfessionQueryHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<ProfessionViewModel>> Handle(GetAllProfessionQuery request, CancellationToken cancellationToken)
+        {
+            return await _context.Professions
+                .Select(x => new ProfessionViewModel()
+                {
+                    Name = x.Name,
+                }
+                ).ToListAsync(cancellationToken);
+        }
+    }
+}
