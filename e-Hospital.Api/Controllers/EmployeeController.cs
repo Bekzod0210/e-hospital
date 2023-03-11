1732721758bc10446bdd4d41315c158d511c3046
Shaxbozbek <Shaxbozbek@VICTUS>
2023-03-11 12:42:16 +0500

    s

--- e-Hospital.Api/Controllers/EmployeeController.cs
+++ e-Hospital.Api/Controllers/EmployeeController.cs
@@ -1,5 +1,8 @@
 ﻿using e_Hospital.Application.UseCases.Admin.Command;
+using e_Hospital.Application.UseCases.Users.Queries;
+using e_Hospital.Domain.Entities;
 using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace e_Hospital.Api.Controllers
@@ -13,12 +16,49 @@ namespace e_Hospital.Api.Controllers
         {
             _mediator = mediator;
         }
-        [HttpPost]
 
-        public async Task<IActionResult> Create (CreateEmployeeCommand command)
+        [Authorize(Policy = "AdminActions")]
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateEmployeeCommand command)
         {
             await _mediator.Send(command);
             return Ok();
         }
+
+        [Authorize(Policy = "AdminActions")]
+        [HttpPut]
+        public async Task<IActionResult> Update(UpdateEmployeeCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok();
+        }
+
+        [Authorize(Policy = "AdminActions")]
+        [HttpDelete("{Id}")]
+        public async Task<IActionResult> Delete(DeleteEmployeeCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok();
+        }
+
+        [Authorize]
+        [HttpGet]
+        public async Task<IActionResult> Get ([FromRoute]GetAllEmployeesQuery query)
+        {
+            var employees = await _mediator.Send(query);
+            if (employees.Count == 0)
+            {
+                return Ok("");
+            }
+            return Ok();
+        }
+        [Authorize]
+        [HttpGet("{Id}")]
+        public async Task<IActionResult> GetById([FromRoute] GetEmployeeByIdQuery query)
+        {
+            await _mediator.Send(query);
+            return Ok();
+        }
+
     }
 }
